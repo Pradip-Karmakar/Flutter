aa96aea2da0867f856ad3aeaba92cf876b5f9aaf
Pradip-Karmakar <skpradip88.pk@gmail.com>
2021-03-20 10:13:08 +0530

    Added : Weather App

---
+++ weather_fortune/lib/screens/loading_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'location_screen.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
+import '../services/weather.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    getLocationData();
+  }
+
+  void getLocationData() async {
+    var weatherLocationData = await WeatherModel().getLocation();
+    Navigator.push(context, MaterialPageRoute(builder: (context){
+        return LocationScreen(locationData: weatherLocationData,);
+    }));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: SpinKitRipple(
+          color: Colors.white,
+          size: 100.0,
+        ),
+      ),
+    );
+  }
+}
